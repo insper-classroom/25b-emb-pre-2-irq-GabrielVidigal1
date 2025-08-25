84a83b116506c6a304118aa55cf14bee6c164718
GabrielVidigal1 <gabrielcv1@al.insper.edu.br>
2025-08-25 16:24:20 -0300

    resto

--- exe4/main.c
+++ exe4/main.c
@@ -1,10 +1,59 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include <stdbool.h>
 #include <stdio.h>
+
+const int BTN_PIN_R = 28;
+const int BTN_PIN_G = 26;
+const int LED_PIN_R = 4;
+const int LED_PIN_G = 6;
+
+volatile bool red_btn_event = false;
+volatile bool green_btn_event = false;
+
+bool red_led_state = false;
+bool green_led_state = false;
+
+void btn_callback(uint gpio, uint32_t events) {
+  if (gpio == BTN_PIN_R && events == GPIO_IRQ_EDGE_FALL) {
+    red_btn_event = true;
+  } else if (gpio == BTN_PIN_G && events == GPIO_IRQ_EDGE_RISE) {
+    green_btn_event = true;
+  }
+}
 
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+
+  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
+                                     &btn_callback);
+  gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_RISE, true);
+
   while (true) {
+    if (red_btn_event) {
+      red_btn_event = false;
+      red_led_state = !red_led_state;
+      gpio_put(LED_PIN_R, red_led_state);
+    }
+
+    if (green_btn_event) {
+      green_btn_event = false;
+      green_led_state = !green_led_state;
+      gpio_put(LED_PIN_G, green_led_state);
+    }
   }
 }
--- exe5/main.c
+++ exe5/main.c
@@ -1,11 +1,49 @@
-#include <stdio.h>
-
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "pico/time.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+const int BTN_PIN = 28;
+const uint32_t LONG_PRESS_THRESHOLD_MS = 800;
+
+volatile uint64_t press_start_time = 0;
+volatile bool event_occurred = false;
+volatile bool is_long_press = false;
+
+void btn_callback(uint gpio, uint32_t events) {
+  if (events & GPIO_IRQ_EDGE_FALL) {
+    press_start_time = time_us_64();
+  } else if (events & GPIO_IRQ_EDGE_RISE) {
+    uint64_t press_duration_us = time_us_64() - press_start_time;
+    if (press_duration_us > LONG_PRESS_THRESHOLD_MS * 1000) {
+      is_long_press = true;
+    } else {
+      is_long_press = false;
+    }
+    event_occurred = true;
+  }
+}
 
 int main() {
-    stdio_init_all();
+  stdio_init_all();
 
-    while (true) {
+  gpio_init(BTN_PIN);
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+  gpio_pull_up(BTN_PIN);
+
+  gpio_set_irq_enabled_with_callback(BTN_PIN,
+                                     GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE,
+                                     true, &btn_callback);
+
+  while (true) {
+    if (event_occurred) {
+      event_occurred = false;
+      if (is_long_press) {
+        printf("Aperto longo!\n");
+      } else {
+        printf("Aperto curto!\n");
+      }
     }
+  }
 }
