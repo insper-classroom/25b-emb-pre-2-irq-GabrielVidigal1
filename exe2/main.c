0073ab1e5cddd24e2198d0eb8943b52aad2d40bb
GabrielVidigal1 <gabrielcv1@al.insper.edu.br>
2025-08-25 16:06:44 -0300

    ex23

--- exe2/main.c
+++ exe2/main.c
@@ -1,42 +1,36 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
-#include <stdio.h>
-
+#include <stdbool.h>
 
 const int BTN_PIN_R = 28;
+const int LED_PIN = 4;
 
-volatile int event_flag = 0;
-
-#define NO_EVENT 0
-#define FALL_EVENT 1
-#define RISE_EVENT 2
+volatile bool btn_pressed_flag = false;
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == GPIO_IRQ_EDGE_FALL) { 
-    event_flag = FALL_EVENT;
-  } else if (events == GPIO_IRQ_EDGE_RISE) { 
-    event_flag = RISE_EVENT;
-  }
+  btn_pressed_flag = true;
 }
-
 
 int main() {
   stdio_init_all();
+
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
   gpio_init(BTN_PIN_R);
   gpio_set_dir(BTN_PIN_R, GPIO_IN);
   gpio_pull_up(BTN_PIN_R);
 
   gpio_set_irq_enabled_with_callback(
-      BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+      BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
+  bool led_is_on = false;
 
   while (true) {
-    if (event_flag == RISE_EVENT) {
-      printf("fall \n");
-      event_flag = NO_EVENT; 
-    } else if (event_flag == RISE_EVENT) {
-      
-      event_flag = NO_EVENT; 
+    if (btn_pressed_flag) {
+      btn_pressed_flag = false;
+      led_is_on = !led_is_on;
+      gpio_put(LED_PIN, led_is_on);
     }
   }
-
 }
--- exe3/main.c
+++ exe3/main.c
@@ -1,39 +1,48 @@
-#include <stdio.h>
-
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
 
+volatile bool btn_r_pressed_flag = false;
+volatile bool btn_g_pressed_flag = false;
+
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) {  // fall edge
-        if (gpio == BTN_PIN_R) {
-            printf("fall red\n");
-        } else if (gpio == BTN_PIN_G) {
-            printf("fall green\n");
-        }
+  if (events == GPIO_IRQ_EDGE_FALL) {
+    if (gpio == BTN_PIN_R) {
+      btn_r_pressed_flag = true;
+    } else if (gpio == BTN_PIN_G) {
+      btn_g_pressed_flag = true;
     }
+  }
 }
 
 int main() {
-    stdio_init_all();
+  stdio_init_all();
 
-    gpio_init(BTN_PIN_R);
-    gpio_set_dir(BTN_PIN_R, GPIO_IN);
-    gpio_pull_up(BTN_PIN_R);
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
 
-    gpio_init(BTN_PIN_G);
-    gpio_set_dir(BTN_PIN_G, GPIO_IN);
-    gpio_pull_up(BTN_PIN_G);
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
 
-    // callback led r (first)
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+  gpio_set_irq_enabled_with_callback(
+      BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
-    // callback led g (nao usar _with_callback)
-    gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
+  gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
-    while (true) {
+  while (true) {
+    if (btn_r_pressed_flag) {
+      btn_r_pressed_flag = false;
+      printf("fall red\n");
     }
+    if (btn_g_pressed_flag) {
+      btn_g_pressed_flag = false;
+      printf("fall green\n");
+    }
+  }
 }
